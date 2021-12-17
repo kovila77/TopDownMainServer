a55a8879daf02199bedea6f7340dceaff6c9a0c7
Дмитрий Ковыляев <354981@mail.ru>
2021-12-17 22:58:26 +0500

    Add project files.

---
+++ TopDownMainServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TopDownMainServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
