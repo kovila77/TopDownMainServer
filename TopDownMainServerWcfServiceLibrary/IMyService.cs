1af5920473c7c6e1ca5eb8e2c46e3842b9fd69dc
olegase123 <olegase123@gmail.com>
2021-12-23 00:04:11 +0500

    Started Wcf Project

---
+++ TopDownMainServerWcfServiceLibrary/IMyService.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+
+namespace TopDownMainServerWcfServiceLibrary {
+	[ServiceContract]
+	public interface IMyService {
+		[OperationContract]
+		string GetServerAddress();
+
+		[OperationContract]
+		string GetServerPort(bool isTCP);
+	}
+}
