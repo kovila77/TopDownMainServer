6cebcde4d27715900dff02a5ae01877386a8e4e0
olegase123 <olegase123@gmail.com>
2021-12-25 18:31:57 +0500

    Deleted WCF Service

--- TopDownMainSeverListener/Services/MatchmakingService.cs
+++ TopDownMainSeverListener/Services/MatchmakingService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
@@ -12,46 +13,42 @@ namespace TopDownMainSeverListener.Services
     {
         public void Run()
         {
-            var listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 9090);
+            var listener = new TcpListener(IPAddress.Parse(ConfigurationManager.AppSettings["ServerMatchmakingAddress"]!), int.Parse(ConfigurationManager.AppSettings["ServerMatchmakingPort"]!));
             listener.Start();
             Matchmaking matchmaking = new Matchmaking();
-            while (true)
-            {
-                TcpClient tcpClient = listener.AcceptTcpClient();
-
-                Task.Run(() =>
-                {
-                    try
-                    {
-                        //using BinaryReader br = new BinaryReader(tcpClient.GetStream());
-                        using BinaryWriter bw = new BinaryWriter(tcpClient.GetStream());
-                        CancellationTokenSource cancellationToken = new CancellationTokenSource();
-                        cancellationToken.CancelAfter(TimeSpan.FromSeconds(60));
-                        var matchmakingResult = matchmaking.GetServerAsync(cancellationToken);
-                        matchmakingResult.Wait();
-
-                        if (matchmakingResult.Result is null)
-                        {
-                            bw.Write("");
-                            bw.Write(-1);
-                            Console.WriteLine($"  {-1}");
-                        }
-                        else
-                        {
-                            bw.Write(matchmakingResult.Result.ServerAddress);
-                            bw.Write(matchmakingResult.Result.ServerPort);
-                            Console.WriteLine($"{matchmakingResult.Result.ServerAddress}   {matchmakingResult.Result.ServerPort}");
-                        }
-                        tcpClient.Close();
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                        tcpClient.Close();
-                    }
-                    tcpClient.Dispose();
-                });
+            while (true) {
+	            SendServerInfoToClient(listener.AcceptTcpClient(), matchmaking);
             }
+        }
+
+        private static async Task SendServerInfoToClient(TcpClient tcpClient, Matchmaking matchmaking) {
+	        await Task.Run(() => {
+		        try {
+			        //using BinaryReader br = new BinaryReader(tcpClient.GetStream());
+			        using BinaryWriter bw = new BinaryWriter(tcpClient.GetStream());
+			        CancellationTokenSource cancellationToken = new CancellationTokenSource();
+			        cancellationToken.CancelAfter(TimeSpan.FromSeconds(60));
+			        var matchmakingResult = matchmaking.GetServerAsync(cancellationToken);
+			        matchmakingResult.Wait();
+
+			        if (matchmakingResult.Result is null) {
+				        bw.Write("");
+				        bw.Write(-1);
+				        Console.WriteLine($"  {-1}");
+			        } else {
+				        bw.Write(matchmakingResult.Result.ServerAddress);
+				        bw.Write(matchmakingResult.Result.ServerPort);
+				        Console.WriteLine($"{matchmakingResult.Result.ServerAddress}   {matchmakingResult.Result.ServerPort}");
+			        }
+
+			        tcpClient.Close();
+		        } catch (Exception e) {
+			        Console.WriteLine(e.Message);
+			        tcpClient.Close();
+		        }
+
+		        tcpClient.Dispose();
+	        });
         }
     }
 }
